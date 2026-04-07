aa97f1cbc1440d8c7e2b8e42bdf4fb345c31214d
lpm0073 <lpm0073@gmail.com>
2026-04-07 08:14:55 -0600

    chore: remove admin dns

--- aws/terraform/kubernetes_ingress/route53.tf
+++ aws/terraform/kubernetes_ingress/route53.tf
@@ -21,39 +21,9 @@ data "kubernetes_service_v1" "traefik" {
   ]
 }
 
-data "aws_elb_hosted_zone_id" "main" {}
-
 # -------------------------------------------------------------------------------------
 # setup DNS for root domain
 # -------------------------------------------------------------------------------------
 data "aws_route53_zone" "root_domain" {
   name = var.root_domain
-}
-
-# -------------------------------------------------------------------------------------
-# setup DNS for admin subdomain
-# -------------------------------------------------------------------------------------
-
-resource "aws_route53_record" "admin_naked" {
-  zone_id = data.aws_route53_zone.services_subdomain.id
-  name    = var.services_subdomain
-  type    = "A"
-
-  alias {
-    name                   = data.kubernetes_service_v1.traefik.status.0.load_balancer.0.ingress.0.hostname
-    zone_id                = data.aws_elb_hosted_zone_id.main.id
-    evaluate_target_health = true
-  }
-}
-
-resource "aws_route53_record" "admin_wildcard" {
-  zone_id = data.aws_route53_zone.services_subdomain.id
-  name    = "*.${var.services_subdomain}"
-  type    = "A"
-
-  alias {
-    name                   = data.kubernetes_service_v1.traefik.status.0.load_balancer.0.ingress.0.hostname
-    zone_id                = data.aws_elb_hosted_zone_id.main.id
-    evaluate_target_health = true
-  }
 }
